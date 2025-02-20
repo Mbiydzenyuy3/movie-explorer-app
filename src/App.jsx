7fb9d31e175a2c4d4fd8f48b4838720ad86ffdc1
leila <mbiydzenyuyeileen@gmail.com>
2025-02-20 13:42:06 +0100

    added changes to app of error boundry file

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Homepage from "./pages/Home";
 import DetailPage from "./pages/MovieDetailsPage";
+import ErrorBoundary from "./components/ErrorCatch/ErrorBoundary";
 
 import { DetailMovieData } from "./context/movieContext";
 
 function App() {
   return (
-    <DetailMovieData>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/details/:id" element={<DetailPage />} />
-        </Routes>
-      </BrowserRouter>
-    </DetailMovieData>
+    <ErrorBoundary>
+      <DetailMovieData>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/details/:id" element={<DetailPage />} />
+          </Routes>
+        </BrowserRouter>
+      </DetailMovieData>
+    </ErrorBoundary>
   );
 }
 
